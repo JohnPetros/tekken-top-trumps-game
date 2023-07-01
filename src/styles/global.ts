8b1292c086c937bc985ac5a3fd1f82fabd44298d
João Pedro Carvalho <93893533+JohnPetros@users.noreply.github.com>
2023-07-01 20:02:22 +0000

    💄 style: Add global e theme styles

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export default createGlobalStyle`${css`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body,
+  #root {
+    width: 100%;
+    height: 100vh;
+
+    font-size: 1.6rem;
+    font-weight: 400;
+    font-family: "Roboto", sans-serif;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+
+    background-color: ${({ theme }) => theme.colors.blue};
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    transition: opacity 0.2s;
+  }
+
+  button:disabled {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+
+  a {
+    font-weight: 500;
+    text-decoration: inherit;
+  }
+`}`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export default {
+  colors: {
+    purple: "#D638DD",
+    blue: "#05041F",
+    red: "#C10108",
+    gray: "#999285",
+    white: "#F9F9F9",
+  },
+};
